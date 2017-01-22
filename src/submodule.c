9f84da7f6fcf3e8bca44dd1d543517b295e73bcd
Edward Thomson <ethomson@github.com>
2017-01-22 00:31:06 +0000

    submodule: provide default NULL to iterator
    Set iterator to NULL to avoid freeing an uninitialized pointer during
    unrelated failures.

--- src/submodule.c
+++ src/submodule.c
@@ -348,10 +348,12 @@ done:
 static int submodules_from_index(git_strmap *map, git_index *idx, git_config *cfg)
 {
 	int error;
-	git_iterator *i;
+	git_iterator *i = NULL;
 	const git_index_entry *entry;
 	git_strmap *names = 0;
+
 	git_strmap_alloc(&names);
+
 	if ((error = load_submodule_names(names, cfg)))
 		goto done;
 
@@ -400,10 +402,12 @@ done:
 static int submodules_from_head(git_strmap *map, git_tree *head, git_config *cfg)
 {
 	int error;
-	git_iterator *i;
+	git_iterator *i = NULL;
 	const git_index_entry *entry;
 	git_strmap *names = 0;
+
 	git_strmap_alloc(&names);
+
 	if ((error = load_submodule_names(names, cfg)))
 		goto done;
 
